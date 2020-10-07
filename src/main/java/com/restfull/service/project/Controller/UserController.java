72a1a956a6878e8af41e23a9c716820eb7511ccc
Esther <esther191299@gmail.com>
2020-10-07 22:26:49 +0200

    Added functionality to read the path(variable)

--- src/main/java/com/restfull/service/project/Controller/UserController.java
+++ src/main/java/com/restfull/service/project/Controller/UserController.java
@@ -3,12 +3,17 @@ package com.restfull.service.project.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("users") // https://localhost:8080/users
+@RequestMapping("/users") // https://localhost:8080/users
 public class UserController {
 
     @GetMapping
     public String getUser() {
         return "getUser was called";
+    }
+
+    @GetMapping(path = "/{userId}")
+    public String getUser1(@PathVariable String userId) {
+        return "getUser was called on id:" + userId;
     }
 
     @PostMapping
